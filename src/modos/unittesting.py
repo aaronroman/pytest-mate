dcae874aad2c262221818c2f0374c8a666ba98b2
Aarón Román <aaronromang@gmail.com>
2023-03-26 17:46:00 +0200

    Update unittesting.py
    misspelling

--- src/modos/unittesting.py
+++ src/modos/unittesting.py
@@ -4,7 +4,7 @@ import re
 def GEN_SYSTEM_PROMPT():
     modo = """You MUST write unittesting code in python.
               I will provide you with the filename and the code.
-              In your response, you MUST return only the test code withing codeblock.
+              In your response, you MUST return only the test code within codeblock.
               Add comments to the code, don't forget to import the modules you need.
               """
     return re.sub(r"\s+", " ", modo)
